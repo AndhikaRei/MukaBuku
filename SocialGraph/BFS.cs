0984b234aa737750ad1eb45e2d210ea7e2c1be48
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-03-15 08:43:44 +0700

    BFS explore friend

--- SocialGraph/BFS.cs
+++ SocialGraph/BFS.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using GraphComponent;
+using ElementQueue;
 using System.Collections.Generic;
 
 public class BFS
 {
     public void friendRecommendation(Graph G, Node person)
     {
-        Dictionary<string,int> recommend = new Dictionary<string,int>();
+        Dictionary<string,int> recommend = new Dictionary<string,int>(); // Key of recommended friend dgn value jumlah mutual friend
         foreach (string friend in person.friends)
         {
-            Node second_person = G.persons.Find(p => p.name.Equals(friend));
+            Node second_person = G.persons.Find(p => p.name.Equals(friend)); // Node setiap person yg sudah berteman dgn person awal
             foreach (string second_friend in second_person.friends)
             {
-                if (second_friend != person.name && !person.friends.Exists(p => p.Equals(second_friend))) // ngecek kalo orang kedua itu bukan person awal dan ga temenan sm person awal
+                if (!second_friend.Equals(person.name) && !person.friends.Exists(p => p.Equals(second_friend))) // Cek orang kedua itu bukan person awal dan ga temenan sm person awal
                 {
                     if (!recommend.ContainsKey(second_friend)) // Kalo blm ada di list, tambah elemen baru
                     {
@@ -37,5 +38,30 @@ public class BFS
 
             //note: konsepnya mau nge"dive" 2 kali buat node person
         }
+    }
+
+    public void exploreFriend(Graph G, Node person, Node second_person)
+    {
+        Queue<ElQueue> queue_person = new Queue<ElQueue>();
+        ElQueue current_person = new ElQueue(person);
+        while (!current_person.getName().Equals(second_person.name)) // Looping sampe ketemu yg sama
+        {
+            foreach (string friend in current_person.person.friends) // Looping untuk semua friend di current person
+            {
+                Node second_Node = G.persons.Find(p => p.name.Equals(friend)); // Mencari Node friend
+                ElQueue next_El = new ElQueue(second_Node); 
+                next_El.addConnection(current_person.person.name); // Menambah urutan connection dari person ke current_person
+                queue_person.Enqueue(next_El); //Masukin ke queue
+            }
+            current_person = queue_person.Dequeue(); // Dequeue untuk next element yg dicek
+        }
+
+        // Print hasil
+        Console.Write(person.name);
+        foreach (string name in current_person.connection)
+        {
+            Console.Write("->" + name);
+        }
+        Console.WriteLine(current_person.getName());
     }
 }
---
+++ SocialGraph/ElQueue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using GraphComponent;
+using System;
+
+namespace ElementQueue
+{
+    class ElQueue
+    {
+        public Node person;
+        public List<string> connection;
+
+        public ElQueue(Node new_person)
+        {
+            person.name = new_person.name; 
+            foreach (string name in new_person.friends)
+            {
+                person.friends.Add(name);
+            }
+        }
+
+        public void addConnection(string name)
+        {
+            connection.Add(name);
+        }
+
+        public string getName()
+        {
+            return person.name;
+        }
+    }
+}
