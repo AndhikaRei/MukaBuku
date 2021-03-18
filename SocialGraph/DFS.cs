4206772b562bf9978ac9c247a7c9d02881e449a1
dwibagus154 <dwibagus154@gmail.com>
2021-03-18 14:24:06 +0700

    dfs

--- SocialGraph/DFS.cs
+++ SocialGraph/DFS.cs
@@ -5,8 +5,23 @@ using System.Collections.Generic;
 
 public class DFS
 {
-    public static string friendRecommendation(Graph G, Node person)
+    public static List<string> exploreFriend(Graph G, Node person, Node second_person, out bool found)
     {
-        return null;
+        found false;
+        // Stack dari simpul yang bisa dikunjungi oleh current_person
+        Stack<Node> simpul_hidup = new Stack<Node>();
+        // Stack dari simpul hidup, digunakan untuk backtracking 
+        Stack<simpul_hidup> Stack_person = new Stack<simpul_hidup>();
+
+        //List string yang telah dikunjungi
+        List<string> has_visited = new List<string>();
+
+        // inisiasi stack dengan elemen awal 
+        Node current_person = person;
+        simpul_hidup.Push(current_person);
+
+
+
+
     }
 }
